c3b5f60204bdca93ddecb921cb88202e4c6911e4
최록산 <choirocksan@choelogsan-ui-MacBookPro.local>
2020-07-07 06:05:24 +0900

    sloved Extra Long Factorilas

--- al1/Extra_Long_Factorials.java
+++ al1/Extra_Long_Factorials.java
@@ -1,4 +1,29 @@
 package al1;
 
+import java.math.BigInteger;
+import java.util.Scanner;
+
 public class Extra_Long_Factorials {
+
+    static void extraLongFactorials(int n) {
+        Factorials(BigInteger.valueOf(1),n);
+
+    }
+    static void Factorials(BigInteger result, int n){
+        if(n>1)
+        Factorials(result.multiply(BigInteger.valueOf(n)),n-1);
+        else
+            System.out.println(result);
+    }
+
+    private static final Scanner scanner = new Scanner(System.in);
+
+    public static void main(String[] args) {
+        int n = scanner.nextInt();
+        scanner.skip("(\r\n|[\n\r\u2028\u2029\u0085])?");
+
+        extraLongFactorials(n);
+
+        scanner.close();
+    }
 }
